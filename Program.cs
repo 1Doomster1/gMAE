07fa9fffd97c74866b4d38b5ad2ce24cea5ef76f
1Doomster1 <semenovicsergej17@gmail.com>
2025-12-17 17:41:15 +0700

    init1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,87 @@
-﻿namespace ConsoleApp5
+﻿using System.Security.AccessControl;
+
+namespace ConsoleApp5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Камень ножницы или бумага?");
+            string choise = Console.ReadLine();
+
+            while (choise == "")
+            {
+                Console.WriteLine("Не выбрал: ");
+                choise = Console.ReadLine();
+            }
+
+            string bot = Random.Shared.Next(1, 4) switch
+            {
+                1 => bot = "Камень",
+                2 => bot = "Ножницы",
+                3 => bot = "Бумага"
+            };
+
+            Console.WriteLine(bot);
+
+            if (choise == bot)
+            { Console.WriteLine("Ничья"); Environment.Exit(0); }
+
+
+            switch (bot)
+            {
+                case "Камень":
+                    {
+                        switch (choise)
+                        {
+                            case "Ножницы":
+                                {
+                                    Console.WriteLine("Победа бота");
+                                    break;
+                                }
+                            case "Бумага":
+                                {
+                                    Console.WriteLine("Победа игрока");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "Ножницы":
+                    {
+                        switch (choise)
+                        {
+                            case "Камень":
+                                {
+                                    Console.WriteLine("Победа игрока");
+                                    break;
+                                }
+                            case "Бумага":
+                                {
+                                    Console.WriteLine("Победа бота");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case "Бумага":
+                    {
+                        switch(choise)
+                        {
+                            case "Ножницы":
+                                {
+                                    Console.WriteLine("Победа игрока");
+                                    break;
+                                }
+                            case "Камень":
+                                {
+                                    Console.WriteLine("Победа бота");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+            }
         }
     }
 }
